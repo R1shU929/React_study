3fee46a355d6f9eb395157d6d02384f233bfeaa4
배혜윤 <charlotte0209@naver.com>
2024-05-19 23:01:55 +0900

    useState

--- section04/src/components/Register.jsx
+++ section04/src/components/Register.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 // 2. 생년월일
 // 3. 국적
 // 4. 자기소개
+// 예이
 const Register = () => {
   const [name, setName] = useState("이름"); // 초기값
   const [birth, setBirth] = useState("");
