fe670db926d9a58d93a5242ea081dd8774e40c51
배혜윤 <charlotte0209@naver.com>
2024-05-25 23:03:53 +0900

    TodoList

--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -1,14 +1,41 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
+import { useState } from "react";
 
-const List = ({todos}) => {
+const List = ({todos,onUpdate,onDelete}) => {
+    const [search,setSearch] = useState("");
+
+    const onChangeSearch = (e) => {
+        setSearch(e.target.value);
+    };
+
+    const getFilteredData = () => {
+        if (search === ""){
+        return todos;
+        }
+        return todos.filter((todo)=>
+        todo.content
+        .toLowerCase()
+        .includes(search.toLowerCase())
+    );
+    };
+
+    const filteredTodos = getFilteredData();
+
     return (
     <div className="List">
         <h4>ToDoList🐰</h4>
-        <input placeholder="검색어를 입력하세요" />
+        <input value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요" />
         <div className="todos_wrapper">
-            {todos.map((todo)=>{
-                return <TodoItem {...todo}/>;
+            {filteredTodos.map((todo)=>{
+                return (
+                <TodoItem key={todo.id} 
+                {...todo} 
+                onUpdate={onUpdate}
+                onDelete={onDelete}/>
+                );
                 })}
         </div>
         </div>
